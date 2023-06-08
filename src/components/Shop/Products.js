f2658efdc75334bfc01d3c5dbe5d4c3954a95638
laubgz <msg13@outlook.es>
2023-06-08 14:09:24 +0200

    add redux-toolkit practice

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,15 +2,33 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+  const PRODUCTS = [
+    {
+      id: 'p1',
+      price: 6,
+      title: 'A book',
+      description: 'The first book ever wrote',
+    },
+    {
+      id: 'p2',
+      price: 12,
+      title: 'Another book',
+      description: 'The second book ever wrote',
+    },
+  ];
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
